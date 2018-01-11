961d654c3edec5b2ecaad94ecd371108a7a0227c
league <league@wts9.attlocal.net>
2018-01-10 20:00:12 -0800

    NasaCountdown StringMethods1

--- src/extra/StringMethods1.java
+++ src/extra/StringMethods1.java
@@ -2,28 +2,44 @@
 //    Level 0
 package extra;
 
+import javax.swing.JOptionPane;
+
+import org.omg.Messaging.SyncScopeHelper;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+			String wiv = "Diego";
+			System.out.println(wiv.toUpperCase());
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+			System.out.println(wiv.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+			System.out.println(wiv.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+			for (int i = 0; i < wiv.length(); i++) {
+				System.out.println(wiv.charAt(i));
+			}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+
+			String input = JOptionPane.showInputDialog("Please input a character in the name of Diego.\nD I E G O");
+			for (int i = 0; i < wiv.length(); i++) {
+				//System.out.println(input);
+				//System.out.println(wiv.charAt(i));
+				if(input.equals(Character.toString(wiv.charAt(i)))) {
+					System.out.println("The letter is at position " +i);
+				}
+			}
+				
+
 	}
 }
 
